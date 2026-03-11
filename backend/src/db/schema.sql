fd80ec9f6ce64d2f81d64fe96197a42da84fc81f
Joshua Thomas <josht2999@gmail.com>
2026-03-11 00:10:35 -0500

    feat(db): added users, groups, and group_members

--- backend/src/db/schema.sql
+++ backend/src/db/schema.sql
@@ -1,9 +1,44 @@
 PRAGMA journal_mode = WAL;
 
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  created_at TEXT NOT NULL DEFAULT (datetime('now'))
+);
+
+CREATE TABLE IF NOT EXISTS groups (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  join_token TEXT UNIQUE NOT NULL,
+
+  created_at TEXT NOT NULL DEFAULT (datetime('now'))
+
+  -- creator_id INTEGER,
+  -- FOREIGN KEY (creator_id) REFERENCES users(id)
+);
+
+CREATE TABLE group_members (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  group_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+
+  FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+
+  UNIQUE(group_id, user_id)
+);
+
 CREATE TABLE IF NOT EXISTS expenses (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  created_at TEXT NOT NULL DEFAULT (datetime('now')),
-  payer TEXT NOT NULL,
+  group_id INTEGER NOT NULL,
+  payer_id INTEGER NOT NULL,
+  -- payer TEXT NOT NULL,
+
   amount_cents INTEGER NOT NULL CHECK (amount_cents > 0),
   description TEXT NOT NULL DEFAULT ''
+
+  created_at TEXT NOT NULL DEFAULT (datetime('now'))
+
+  FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE,
+  FOREIGN KEY (payer_id) REFERENCES users(id)
 );
